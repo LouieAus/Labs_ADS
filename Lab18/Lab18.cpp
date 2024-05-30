e79148227ca6bea2c5b3f3c5bad5df8d508edaa4
Lvov <i.lvov81@mail.ru>
2024-05-31 00:36:57 +0200

    Lab18 added

---
+++ Lab18/Lab18.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> FindArraySumm(std::vector<int> nums, int summ)
+{
+	std::sort(nums.begin(), nums.end(), [](int a, int b) { return a < b; });
+
+	std::vector<int> result;
+	int temp_summ = 0;
+
+	for (auto i = 0; i != nums.size(); i++)
+	{
+		int temp1 = summ - (temp_summ + nums[i]);
+		int temp2 = summ - temp_summ;
+
+		if (temp1 == 0)
+		{
+			result.push_back(nums[i]);
+			return result;
+		}
+		else if (temp1 > 0 && temp1 < temp2)
+		{
+			temp_summ += nums[i];
+			result.push_back(nums[i]);
+		}
+	}
+
+	return result;
+}
+
+int main()
+{
+	std::vector<int> data = { 22, 7, 1, 4, 9, 10, 4, 38 };
+	std::vector<int> res = FindArraySumm(data, 18);
+	
+	for (int r : res)
+		std::cout << r << '\t';
+}
